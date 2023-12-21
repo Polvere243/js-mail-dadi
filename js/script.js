fd845e7ad5b6bbbf1cd366054c0b539d27283c18
Polvere243 <danielercoli@gmail.com>
2023-12-21 17:36:57 +0100

    generated two random numbers

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
-console.log("JS OK");
+const max = 6;
+const min = 1;
+
+let userDie = Math.floor(Math.random() * (max + 1 - min)) + min;
+let computerDie = Math.floor(Math.random() * (max + 1 - min)) + min;
+
+console.log(userDie, computerDie);
