65232ec1631192548c05ad8cbe4a839b92916d59
Polvere243 <danielercoli@gmail.com>
2023-12-21 17:43:14 +0100

    compared the numbers and logged a message in console

--- js/script.js
+++ js/script.js
@@ -1,7 +1,14 @@
+// dichiaro due costanti: una per il valore minimo e una per il valore massimo dei dadi
 const max = 6;
 const min = 1;
-
+// faccio generare due numeri casuali da 1 a 6
 let userDie = Math.floor(Math.random() * (max + 1 - min)) + min;
 let computerDie = Math.floor(Math.random() * (max + 1 - min)) + min;
 
 console.log(userDie, computerDie);
+// collaziono i due risultati
+if(userDie > computerDie){
+    console.log("hai vinto");
+} else {
+    console.log("hai perso");
+}
