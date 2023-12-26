b4d63c9fa8f4a273f708ac31107968583bea9c36
Polvere243 <danielercoli@gmail.com>
2023-12-26 12:32:01 +0100

    got element from the DOM and created an array

--- js/login.js
+++ js/login.js
@@ -1,1 +1,3 @@
-console.log("JS OK");
+const button = document.getElementById("check-me");
+const form = document.getElementById("email-form");
+const allowedEmails = ["pincopanco@gmail.com", "danielercoli@gmail.com", "bepperampons@gamil.com"];
