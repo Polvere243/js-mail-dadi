9504dbb2ef81ca6551bc2c6dd6f1557797db2a6d
Polvere243 <danielercoli@gmail.com>
2023-12-26 12:40:50 +0100

    got the input value by a click on the button

--- js/login.js
+++ js/login.js
@@ -1,3 +1,12 @@
+// recupero gli elementi dalla pagina
 const button = document.getElementById("check-me");
 const form = document.getElementById("email-form");
 const allowedEmails = ["pincopanco@gmail.com", "danielercoli@gmail.com", "bepperampons@gamil.com"];
+// al click sul bottone recupero il valore dall'input
+
+button.addEventListener ("click", function () {
+    const userEmail = form.value.trim();
+    console.log(userEmail);
+
+    
+})
